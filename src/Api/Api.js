971b03c28ad5e3e22576b1c98b801e2a47bd95f1
Dmitriiplastinin-1231 <dimanplastinen@gmail.com>
2023-06-04 16:57:30 +0500

    replace fetch to axios

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,10 +1,10 @@
-
+import axios from "axios";
 
 
 const getPizzas = () => {
-    return fetch('https://64734f45d784bccb4a3c7205.mockapi.io/item')
+    return axios.get('https://64734f45d784bccb4a3c7205.mockapi.io/item')
         .then((response) => {
-            return response.json();
+            return response.data;
         })
 }
 
